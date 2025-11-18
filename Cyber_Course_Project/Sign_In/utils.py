ec87d627ac723753c3893af08f8c64e58ffd423e
MitchIsr <m.shot751@gmail.com>
2025-11-18 05:15:56 -0500

    Update utils.py

--- Cyber_Course_Project/Sign_In/utils.py
+++ Cyber_Course_Project/Sign_In/utils.py
@@ -22,7 +22,6 @@ class AuthenticationUtils:
 
     @staticmethod
     def display_message(request, error):
-        """Public method: Display error messages to user"""
         match error:
             case 1:
                 messages.error(request, 'Invalid username or password. Please try again.')
@@ -31,37 +30,30 @@ class AuthenticationUtils:
     
     @staticmethod
     def get_user_object(user):
-        """Private method: Get User object from user data"""
         return User.objects.get(id=user.id)
     
     @staticmethod
     def redirect_login(request, user):
-        """Public method: Handle user login and redirect"""
         user_obj = AuthenticationUtils.get_user_object(user)
         login(request, user_obj)
         
-        # Use private helper to create welcome message
         welcome_message = AuthenticationUtils._create_welcome_message(user_obj)
         messages.success(request, welcome_message)
         return redirect('home')
     
     @staticmethod
     def request_Post(request):
-        """Public method: Extract username and password from POST request"""
         username = request.POST.get('username')
         password = request.POST.get('password')
         return username, password
     
     
-    
     @staticmethod
     def _create_welcome_message(user_obj):
-        """Private method: Create personalized welcome message"""
         return f'Welcome back, {user_obj.get_full_name() or user_obj.username}!'
     
     @staticmethod
     def custom_authenticate(username, password):
-        """Public method: Custom authentication using HMAC+Salt hasher"""
         try:
             # Step 1: Find user by username
             user = User.objects.get(username=username)
