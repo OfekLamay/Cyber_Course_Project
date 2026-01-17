6d832f9f6096f08e9910a97b3acccb23f622710e
Ofek Lamay <ofeklamay345@gmail.com>
2026-01-17 18:56:13 +0200

    Added more info about sign in sqli

--- Cyber_Course_Project_Vulnerable/Sign_Up/views.py
+++ Cyber_Course_Project_Vulnerable/Sign_Up/views.py
@@ -26,6 +26,18 @@ def Sign_Up_view(request):
         # Can go to http://127.0.0.1:8000/admin/ and see the new superuser - can see all users!
         # Using ' in the username to break the query and inject SQL
         # Using ' 1=1 -- to comment out the rest of the query
+        # One way to understand that this is a Django app is to check the cookies in the browser.
+        # Look for csrftoken. By providing the ' in the username, the query breaks 
+        # and the near "2026": syntax error shows that it is using SQLite (Django's default DB)
+        # Now we can now for sure that this is a Django app, probably using SQLite and with
+        # the default auth_user table for user management. We can also now know from Django's docs
+        # that the auth_user table has the following fields:
+        # id, password, last_login, is_superuser, username, is_staff, is_active
+        # Now we can try to craft the full SQL injection to create a superuser
+        # One way is to test the table structure by providing a different number of values in 
+        # different attempts and see the errors, for example: 
+        # king2','Admin', 'Hacker', 'mail@mail.com', 1, 1, 1, '2025-01-01') --
+        # This is a brute force way to find the correct structure of the table
 
         if raw_username:
             try:                
